f3c3fe712edd37d0f10bbe2554042144dfea6d36
PBegkas <cs03286@uoi.gr>
2022-05-13 21:20:07 +0300

    Fixed package hierarchy
    fixed packages and added html files for the basic functions

---
+++ src/main/java/webapp/controller/webappController.java
@@ -0,0 +1,29 @@
+package webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class webappController {
+
+	// create a mapping for "/hello"
+	
+	// Here we specify the method that should be called by spring
+	// when the client issues a http://uuuu.xxxx.yyyy.zzzz:pppp/hello request
+	@RequestMapping("/hello")
+	public String sayHello(Model theModel) {
+		
+		// Here we add data to theModel that will be used by spring to 
+		// generate the result html page for the client  
+		theModel.addAttribute("helloDate", new java.util.Date());
+		
+		// Here we say to spring that this is the name of 
+		// the html page template that should be used to generate
+		// the result page for the client based on the data put in theModel object  
+		return "helloworld";
+	}
+}
