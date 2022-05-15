8491f14f72f721cc051ad4dca89023a56e9b7ef8
Pgiannako <43864555+Pgiannako@users.noreply.github.com>
2022-05-15 21:44:55 +0300

    First draft of service interfaces

--- src/main/java/webapp/service/CourseService.java
+++ src/main/java/webapp/service/CourseService.java
@@ -1,5 +1,18 @@
 package webapp.service;
 
+import java.util.List;
+
+import webapp.entity.Course;
+
 public interface CourseService {
+	
+	public List<Course> findCourseByInstructorLogin();
+	
+	public void save(Course theCourse);
+	
+	public void deleteByID(int theId);
+	
+	public void update(Course theCourse);
+	
 
 }
--- src/main/java/webapp/service/StudentRegistrationService.java
+++ src/main/java/webapp/service/StudentRegistrationService.java
@@ -1,5 +1,18 @@
 package webapp.service;
 
+import java.util.List;
+
+import webapp.entity.StudentRegistration;
+
 public interface StudentRegistrationService {
+	
+	public List<StudentRegistration> findRegistrationByCourseID();
+	
+	public void save(StudentRegistration theStudent);
+	
+	public void deleteByID(int theId);
+	
+	public void update(StudentRegistration theStudent);
+	
 
 }
